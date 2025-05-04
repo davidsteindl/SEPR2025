fb4ac19fb415132fa91d6a39d22f8dc4532b2b04
Markus <5242@htl.rennweg.at>
2025-05-04 23:55:54 +0200

    #30 - UserValidator programmiert

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/validators/UserValidator.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/validators/UserValidator.java
@@ -1,4 +1,26 @@
 package at.ac.tuwien.sepr.groupphase.backend.service.validators;
 
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
+import jakarta.validation.ValidationException;
+
+import java.time.LocalDateTime;
+
 public class UserValidator {
+
+    public UserValidator() {
+    }
+
+
+    public void validateForRegistration(ApplicationUser user) throws ValidationException {
+
+
+        if (!user.getEmail().contains("@")) {
+            throw new ValidationException("The email must contain a @");
+        }
+
+        if (user.getDateOfBirth().isAfter(LocalDateTime.now())) {
+            throw new ValidationException("The Birthdate must be in the past");
+        }
+
+    }
 }
