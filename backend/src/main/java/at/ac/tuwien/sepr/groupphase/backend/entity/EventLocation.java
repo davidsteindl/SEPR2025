264f981613ca8179d14246c600c6d02613eab9b1
Filip Grgic <e12324225@student.tuwien.ac.at>
2025-05-05 01:22:15 +0200

    #176 - Entitäten 'Event' und 'EventLocation' erstellt

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/EventLocation.java
@@ -0,0 +1,212 @@
+package at.ac.tuwien.sepr.groupphase.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class EventLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LocationType type;
+
+    @Column(nullable = false, length = 100)
+    private String country;
+
+    @Column(nullable = false, length = 100)
+    private String city;
+
+    @Column(nullable = false, length = 100)
+    private String street;
+
+    @Column(nullable = false, length = 100)
+    private String postalCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocationType getType() {
+        return type;
+    }
+
+    public void setType(LocationType type) {
+        this.type = type;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof EventLocation eventLocation)) {
+            return false;
+        }
+        return Objects.equals(id, eventLocation.id)
+            && Objects.equals(name, eventLocation.name)
+            && type == eventLocation.type
+            && Objects.equals(country, eventLocation.country)
+            && Objects.equals(city, eventLocation.city)
+            && Objects.equals(street, eventLocation.street)
+            && Objects.equals(postalCode, eventLocation.postalCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type, country, city, street, postalCode);
+    }
+
+    @Override
+    public String toString() {
+        return "EventLocation{"
+            + "id=" + id
+            + ", name='" + name + '\''
+            + ", type=" + (type != null ? type.getDisplayName() : "null")
+            + ", country='" + country + '\''
+            + ", city='" + city + '\''
+            + ", street='" + street + '\''
+            + ", postalCode='" + postalCode + '\''
+            + '}';
+    }
+
+    public enum LocationType {
+        STADIUM("Stadium"),
+        FESTIVAL_GROUND("Festival Ground"),
+        HALL("Hall"),
+        OPERA("Opera"),
+        THEATER("Theater"),
+        CLUB("Club"),
+        OTHER("Other");
+
+        private final String displayName;
+
+        LocationType(String displayName) {
+            this.displayName = displayName;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
+    }
+
+    public static final class EventLocationBuilder {
+        private Long id;
+        private String name;
+        private LocationType type;
+        private String country;
+        private String city;
+        private String street;
+        private String postalCode;
+
+        private EventLocationBuilder() {
+        }
+
+        public static EventLocationBuilder anEventLocation() {
+            return new EventLocationBuilder();
+        }
+
+        public EventLocationBuilder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public EventLocationBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public EventLocationBuilder withType(LocationType type) {
+            this.type = type;
+            return this;
+        }
+
+        public EventLocationBuilder withCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public EventLocationBuilder withCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public EventLocationBuilder withStreet(String street) {
+            this.street = street;
+            return this;
+        }
+
+        public EventLocationBuilder withPostalCode(String postalCode) {
+            this.postalCode = postalCode;
+            return this;
+        }
+
+        public EventLocation build() {
+            EventLocation eventLocation = new EventLocation();
+            eventLocation.setId(id);
+            eventLocation.setName(name);
+            eventLocation.setType(type);
+            eventLocation.setCountry(country);
+            eventLocation.setCity(city);
+            eventLocation.setStreet(street);
+            eventLocation.setPostalCode(postalCode);
+            return eventLocation;
+        }
+    }
+}
