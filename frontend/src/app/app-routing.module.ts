922798800d5f1e9030704a69a6db2fdd905e12ee
Markus <5242@htl.rennweg.at>
2025-05-04 10:49:48 +0200

    Router angepasst auf die Namensänderung

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'termsandservice', component: TermsandconditionsComponent},
+  {path: 'termsandconditions', component: TermsandconditionsComponent},
   {path: 'message', canActivate: mapToCanActivate([AuthGuard]), component: MessageComponent}
 ];
 
