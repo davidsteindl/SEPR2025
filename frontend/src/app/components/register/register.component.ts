44246c1a7af384f1eb551e3630ba04902c275165
Markus <5242@htl.rennweg.at>
2025-05-05 10:15:14 +0200

    #23 - Constructor für register.component.ts geschrieben

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {UntypedFormBuilder, UntypedFormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 
 
@@ -8,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  registerForm: UntypedFormGroup;
+  // After first submission attempt, form validation will start
+  submitted = false;
+  // Error flag
+  error = false;
+  errorMessage = '';
+
+  constructor(private formBuilder: UntypedFormBuilder, private authService: AuthService, private router: Router) {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.maxLength(100)]],
+      lastName: ['', [Validators.required, Validators.maxLength(100)]],
+      dateOfBirth: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.maxLength(100), Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
 
 }
