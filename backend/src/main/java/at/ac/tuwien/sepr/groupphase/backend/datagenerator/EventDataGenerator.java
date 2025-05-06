bdaa4ef292ca65aaa96d09c205ed50a19c951ede
Filip Grgic <e12324225@student.tuwien.ac.at>
2025-05-06 12:39:56 +0200

    #176 - Entitäten 'Show' und 'Artist' wurden samt Repositories angelegt und in 'EventDataGenerator' eingebunden. 'application.yml' wurde angepasst.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/EventDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/EventDataGenerator.java
@@ -1,9 +1,14 @@
 package at.ac.tuwien.sepr.groupphase.backend.datagenerator;
 
+import at.ac.tuwien.sepr.groupphase.backend.entity.Artist;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Event;
 import at.ac.tuwien.sepr.groupphase.backend.entity.EventLocation;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Show;
+import at.ac.tuwien.sepr.groupphase.backend.repository.ArtistRepository;
 import at.ac.tuwien.sepr.groupphase.backend.repository.EventLocationRepository;
 import at.ac.tuwien.sepr.groupphase.backend.repository.EventRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.ShowRepository;
+import io.jsonwebtoken.lang.Collections;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +17,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.lang.invoke.MethodHandles;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Profile("generateData")
 @Component
@@ -20,14 +27,21 @@ public class EventDataGenerator {
 
     private final EventRepository eventRepository;
     private final EventLocationRepository eventLocationRepository;
+    private final ShowRepository showRepository;
+    private final ArtistRepository artistRepository;
 
     private static final int NUMBER_OF_EVENTS_TO_GENERATE = 5;
     private static final int NUMBER_OF_LOCATIONS_TO_GENERATE = 5;
+    private static final int NUMBER_OF_SHOWS_TO_GENERATE = 10;
+    private static final int NUMBER_OF_ARTISTS_TO_GENERATE = 20;
 
     @Autowired
-    public EventDataGenerator(EventRepository eventRepository, EventLocationRepository eventLocationRepository) {
+    public EventDataGenerator(EventRepository eventRepository, EventLocationRepository eventLocationRepository, ShowRepository showRepository,
+                              ArtistRepository artistRepository) {
         this.eventRepository = eventRepository;
         this.eventLocationRepository = eventLocationRepository;
+        this.showRepository = showRepository;
+        this.artistRepository = artistRepository;
     }
 
     @PostConstruct
@@ -37,8 +51,9 @@ public class EventDataGenerator {
         } else {
             LOGGER.debug("generating {} EventLocation entries", NUMBER_OF_LOCATIONS_TO_GENERATE);
             for (int i = 0; i < NUMBER_OF_LOCATIONS_TO_GENERATE; i++) {
+                long id = -(NUMBER_OF_LOCATIONS_TO_GENERATE - i);
                 EventLocation eventLocation = EventLocation.EventLocationBuilder.anEventLocation()
-                    .withId((long) i - NUMBER_OF_LOCATIONS_TO_GENERATE - 1)
+                    .withId(id)
                     .withName("Location " + i)
                     .withType(EventLocation.LocationType.OPERA)
                     .withCountry("Country " + i)
@@ -56,15 +71,74 @@ public class EventDataGenerator {
         } else {
             LOGGER.debug("generating {} Event entries", NUMBER_OF_EVENTS_TO_GENERATE);
             for (int i = 0; i < NUMBER_OF_EVENTS_TO_GENERATE; i++) {
-                EventLocation eventLocation = eventLocationRepository.findById((long) i - NUMBER_OF_LOCATIONS_TO_GENERATE - 1);
-                if (eventLocation != null) {
+                long id = -(NUMBER_OF_EVENTS_TO_GENERATE - i);
+                Optional<EventLocation> eventLocation = eventLocationRepository.findById((long) -(NUMBER_OF_LOCATIONS_TO_GENERATE - (i % NUMBER_OF_LOCATIONS_TO_GENERATE)));
+                if (eventLocation.isPresent()) {
                     Event event = Event.EventBuilder.anEvent()
+                        .withId(id)
                         .withName("Event " + i)
                         .withCategory(Event.EventCategory.CLASSICAL)
-                        .withLocation(eventLocation)
+                        .withLocation(eventLocation.get())
                         .build();
                     LOGGER.debug("saving event {}", event);
                     eventRepository.save(event);
+                }
+            }
+        }
+
+        if (!artistRepository.findAll().isEmpty()) {
+            LOGGER.debug("artists already generated");
+        } else {
+            LOGGER.debug("generating {} Artist entries", NUMBER_OF_ARTISTS_TO_GENERATE);
+            for (int i = 0; i < NUMBER_OF_ARTISTS_TO_GENERATE; i++) {
+                long id = -(NUMBER_OF_ARTISTS_TO_GENERATE - i);
+                Artist artist = Artist.ArtistBuilder.anArtist()
+                    .withId(id)
+                    .withFirstname("Firstname " + i)
+                    .withLastname("Lastname " + i)
+                    .withStagename("Stagename " + i)
+                    .build();
+                LOGGER.debug("saving artist {}", artist);
+                artistRepository.save(artist);
+            }
+        }
+
+        if (!showRepository.findAll().isEmpty()) {
+            LOGGER.debug("shows already generated");
+        } else {
+            LOGGER.debug("generating {} Show entries", NUMBER_OF_SHOWS_TO_GENERATE);
+            int artistIndex = 0;
+            for (int i = 0; i < NUMBER_OF_EVENTS_TO_GENERATE; i++) {
+                Optional<Event> eventOpt = eventRepository.findById((long) -(NUMBER_OF_EVENTS_TO_GENERATE - i));
+                if (eventOpt.isPresent()) {
+                    Event event = eventOpt.get();
+                    for (int j = 0; j < 2; j++) { // 2 shows per event
+                        long showId = -(NUMBER_OF_SHOWS_TO_GENERATE - (i * 2 + j));
+                        Optional<Artist> artist1Opt = artistRepository.findById((long) -(NUMBER_OF_ARTISTS_TO_GENERATE - artistIndex));
+                        Optional<Artist> artist2Opt = artistRepository.findById((long) -(NUMBER_OF_ARTISTS_TO_GENERATE - (artistIndex + 1)));
+                        if (artist1Opt.isPresent() && artist2Opt.isPresent()) {
+                            Artist artist1 = artist1Opt.get();
+                            Artist artist2 = artist2Opt.get();
+
+                            Show show = Show.ShowBuilder.aShow()
+                                .withId(showId)
+                                .withDuration(120)
+                                .withDateTime(LocalDateTime.of(2025, 6, i + 1, 12 + j * 4, 0))
+                                .withEvent(event)
+                                .withArtists(Collections.setOf(artist1, artist2))
+                                .build();
+
+
+                            artist1.setShows(Collections.setOf(show));
+                            artist2.setShows(Collections.setOf(show));
+
+                            LOGGER.debug("saving show {}", show);
+                            showRepository.save(show);
+                            artistRepository.save(artist1);
+                            artistRepository.save(artist2);
+                        }
+                        artistIndex += 2;
+                    }
                 }
             }
         }
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ArtistRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ArtistRepository.java
@@ -1,9 +1,9 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository;
 
-import at.ac.tuwien.sepr.groupphase.backend.entity.Event;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
 }
