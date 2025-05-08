c58fe243f5218cfc3c7e8a3f200cd91b084b8762
Filip Grgic <e12324225@student.tuwien.ac.at>
2025-05-08 23:35:42 +0200

    #176 - Methoden für Artist und Show in EventService hinzugefügen

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/EventService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/EventService.java
@@ -1,7 +1,9 @@
 package at.ac.tuwien.sepr.groupphase.backend.service;
 
+import at.ac.tuwien.sepr.groupphase.backend.entity.Artist;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Event;
 import at.ac.tuwien.sepr.groupphase.backend.entity.EventLocation;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Show;
 
 import java.util.List;
 
@@ -32,7 +34,7 @@ public interface EventService {
     /**
      * Returns the location of the event with the given id.
      *
-     * @param eventId the id of the event
+     * @param eventId the id of the event to retrieve
      * @return the event location, or null if no such event location exists
      */
     EventLocation getEventLocationById(Long eventId);
@@ -51,4 +53,50 @@ public interface EventService {
      * @return the saved event location
      */
     EventLocation createEventLocation(EventLocation eventLocation);
+
+    /**
+     * Returns the artist with the given ID.
+     *
+     * @param id the ID of the artist to retrieve
+     * @return the artist with the given ID, or null if no such artist exists
+     */
+    Artist getArtistById(Long id);
+
+    /**
+     * Returns all artists.
+     *
+     * @return a list of all artists
+     */
+    List<Artist> getAllArtists();
+
+    /**
+     * Saves the given artist.
+     *
+     * @param artist the artist to save
+     * @return the saved artist
+     */
+    Artist createArtist(Artist artist);
+
+    /**
+     * Returns the show with the given ID.
+     *
+     * @param id the ID of the show to retrieve
+     * @return the show with the given ID, or null if no such show exists
+     */
+    Show getShowById(Long id);
+
+    /**
+     * Returns all shows.
+     *
+     * @return a list of all shows
+     */
+    List<Show> getAllShows();
+
+    /**
+     * Saves the given show.
+     *
+     * @param show the show to save
+     * @return the saved show
+     */
+    Show createShow(Show show);
 }
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/EventServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/EventServiceImpl.java
@@ -1,9 +1,13 @@
 package at.ac.tuwien.sepr.groupphase.backend.service.impl;
 
+import at.ac.tuwien.sepr.groupphase.backend.entity.Artist;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Event;
 import at.ac.tuwien.sepr.groupphase.backend.entity.EventLocation;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Show;
+import at.ac.tuwien.sepr.groupphase.backend.repository.ArtistRepository;
 import at.ac.tuwien.sepr.groupphase.backend.repository.EventLocationRepository;
 import at.ac.tuwien.sepr.groupphase.backend.repository.EventRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.ShowRepository;
 import at.ac.tuwien.sepr.groupphase.backend.service.EventService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,11 +22,16 @@ public class EventServiceImpl implements EventService {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final EventRepository eventRepository;
     private final EventLocationRepository eventLocationRepository;
+    private final ArtistRepository artistRepository;
+    private final ShowRepository showRepository;
 
     @Autowired
-    public EventServiceImpl(EventRepository eventRepository, EventLocationRepository eventLocationRepository) {
+    public EventServiceImpl(EventRepository eventRepository, EventLocationRepository eventLocationRepository,
+                            ArtistRepository artistRepository, ShowRepository showRepository) {
         this.eventRepository = eventRepository;
         this.eventLocationRepository = eventLocationRepository;
+        this.artistRepository = artistRepository;
+        this.showRepository = showRepository;
     }
 
     @Override
@@ -59,5 +68,41 @@ public class EventServiceImpl implements EventService {
     public EventLocation createEventLocation(EventLocation eventLocation) {
         LOGGER.info("Save event location {}", eventLocation);
         return eventLocationRepository.save(eventLocation);
+    }
+
+    @Override
+    public Artist getArtistById(Long id) {
+        LOGGER.info("Find artist with id {}", id);
+        return artistRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Artist> getAllArtists() {
+        LOGGER.info("Get all artists");
+        return artistRepository.findAll();
+    }
+
+    @Override
+    public Artist createArtist(Artist artist) {
+        LOGGER.info("Save artist {}", artist);
+        return artistRepository.save(artist);
+    }
+
+    @Override
+    public Show getShowById(Long id) {
+        LOGGER.info("Find show with id {}", id);
+        return showRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Show> getAllShows() {
+        LOGGER.info("Get all shows");
+        return showRepository.findAll();
+    }
+
+    @Override
+    public Show createShow(Show show) {
+        LOGGER.info("Save show {}", show);
+        return showRepository.save(show);
     }
 }
