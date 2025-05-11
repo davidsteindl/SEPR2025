b1e96ed1c9f35fe969f2a360d7abe6c24a4c53bd
TheSte1n <davidsteindl0@gmail.com>
2025-05-11 22:44:22 +0200

    #150 Tests für CustomSearchService, SearchEventEndpoint und SearchValidator geschrieben

---
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/integrationtest/SearchEventEndpointTest.java
@@ -0,0 +1,46 @@
+package at.ac.tuwien.sepr.groupphase.backend.integrationtest;
+
+
+import at.ac.tuwien.sepr.groupphase.backend.repository.EventRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.ShowRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SearchEventEndpointTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EventRepository eventRepo;
+
+    @Autowired
+    private ShowRepository showRepo;
+
+    @BeforeEach
+    void setUp() {
+        showRepo.deleteAll();
+        eventRepo.deleteAll();
+    }
+
+    @Test
+    void searchNoDataReturnsEmptyContent() throws Exception {
+        mockMvc.perform(get("/api/v1/events/search"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.content").isArray())
+            .andExpect(jsonPath("$.content").isEmpty());
+    }
+}
