7c0f275d540e0fd9c2eb91a8b60db2330bf59511
12312839 <e12312839@student.tuwien.ac.at>
2025-05-11 12:15:19 +0200

    #180 - Search-Component Code einheitlich gestaltet

--- frontend/src/app/components/search/search.component.ts
+++ frontend/src/app/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { CommonModule } from '@angular/common';
 
 @Component({
