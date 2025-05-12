176eb38a7717e436b158341f3740aa1bcc89fb4c
12312839 <e12312839@student.tuwien.ac.at>
2025-05-12 03:34:19 +0200

    #133 - ArtistSearchDto gecodet

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/ArtistSearchDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/ArtistSearchDto.java
@@ -1,4 +1,49 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
 public class ArtistSearchDto {
+    private String firstname;
+    private String lastname;
+    private String stagename;
+
+    public ArtistSearchDto() {
+    }
+
+    public ArtistSearchDto(String firstname, String lastname, String stagename) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.stagename = stagename;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getStagename() {
+        return stagename;
+    }
+
+    public void setStagename(String stagename) {
+        this.stagename = stagename;
+    }
+
+    @Override
+    public String toString() {
+        return "ArtistSearchDto{" +
+            "firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            ", stagename='" + stagename + '\'' +
+            '}';
+    }
 }
