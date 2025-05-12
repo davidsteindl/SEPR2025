f83ff8106ee5dd45716fb656a9a1377828d6cfd5
TheSte1n <davidsteindl0@gmail.com>
2025-05-12 07:33:41 +0200

    #151 Methode searchWithDataReturnsContent zu SearchEventEndpointTest hinzugefügt

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/integrationtest/SearchEventEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/integrationtest/SearchEventEndpointTest.java
@@ -3,6 +3,8 @@ package at.ac.tuwien.sepr.groupphase.backend.integrationtest;
 
 import at.ac.tuwien.sepr.groupphase.backend.config.properties.SecurityProperties;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.SearchEventEndpoint;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.EventSearchDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.EventSearchResultDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.ShowMapper;
 import at.ac.tuwien.sepr.groupphase.backend.service.SearchService;
 import at.ac.tuwien.sepr.groupphase.backend.service.ShowService;
@@ -51,5 +53,36 @@ public class SearchEventEndpointTest {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.content").isArray())
             .andExpect(jsonPath("$.content").isEmpty());
+    }
+    @Test
+    @WithMockUser(username = "user", roles = "USER")
+    void searchWithDataReturnsContent() throws Exception {
+        EventSearchResultDto resultDto = EventSearchResultDto.EventSearchResultDtoBuilder
+            .anEventSearchResultDto()
+            .id(1L)
+            .name("Test Event")
+            .category("Rock")
+            .locationId(42L)
+            .duration(120)
+            .description("Description")
+            .build();
+
+        when(searchService.searchEvents(any(EventSearchDto.class)))
+            .thenReturn(new PageImpl<>(List.of(resultDto)));
+
+        mockMvc.perform(get("/api/v1/events/search")
+                .param("name", "Test Event")
+                .param("page", "0")
+                .param("size", "1")
+            )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.content").isArray())
+            .andExpect(jsonPath("$.content.length()").value(1))
+            .andExpect(jsonPath("$.content[0].id").value(1))
+            .andExpect(jsonPath("$.content[0].name").value("Test Event"))
+            .andExpect(jsonPath("$.content[0].category").value("Rock"))
+            .andExpect(jsonPath("$.content[0].locationId").value(42))
+            .andExpect(jsonPath("$.content[0].duration").value(120))
+            .andExpect(jsonPath("$.content[0].description").value("Description"));
     }
 }
