dfa4b7fa557e71d098426eec9349ab07f0310559
12312839 <e12312839@student.tuwien.ac.at>
2025-05-15 13:08:50 +0200

    #198 - event-overview gecodet

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ import { CreateShowComponent } from './components/create-content/create-show/cre
 import { CreateLocationComponent } from './components/create-content/create-location/create-location.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { ArtistEventsComponent } from './components/artist-events/artist-events.component';
+import {EventOverviewComponent} from "./components/event-overview/event-overview.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -39,7 +40,8 @@ const routes: Routes = [
   { path: 'create-show', component: CreateShowComponent, canActivate: [AdminGuard] },
   { path: 'create-location', component: CreateLocationComponent, canActivate: [AdminGuard] },
   { path: 'admin', component: AdminComponent },
-  { path: 'artists/:id/events', component: ArtistEventsComponent }
+  { path: 'artists/:id/events', component: ArtistEventsComponent },
+  { path: 'events/:id/overview', component: EventOverviewComponent }
 ];
 
 @NgModule({
--- frontend/src/app/components/event-overview/event-overview.component.ts
+++ frontend/src/app/components/event-overview/event-overview.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EventService } from '../../services/event.service';
+import { Event } from '../../dtos/event';
+import { Show } from '../../dtos/show';
+import { EventWithShows } from '../../dtos/event';
 
 @Component({
   selector: 'app-event-overview',
-  imports: [],
   templateUrl: './event-overview.component.html',
-  styleUrl: './event-overview.component.scss'
 })
-export class EventOverviewComponent {
+export class EventOverviewComponent implements OnInit {
+  event: Event | null = null;
+  shows: Show[] = [];
 
+  constructor(private route: ActivatedRoute, private eventService: EventService) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.eventService.getEventWithShows(id).subscribe({
+      next: (data: EventWithShows) => {
+        this.event = data.event;
+        this.shows = data.shows;
+      },
+      error: (err) => console.error('Failed to load event overview', err)
+    });
+  }
 }
