74e02bf90e4ad01d44c4686d37dc4a8d9a39c0bf
12312839 <e12312839@student.tuwien.ac.at>
2025-05-15 22:50:25 +0200

    #198 - design verbessert

--- frontend/src/app/components/event-overview/event-overview.component.ts
+++ frontend/src/app/components/event-overview/event-overview.component.ts
@@ -4,7 +4,11 @@ import { EventService } from '../../services/event.service';
 import { Event } from '../../dtos/event';
 import { Show } from '../../dtos/show';
 import { EventWithShows } from '../../dtos/event';
-import {NgForOf, NgIf} from "@angular/common";
+import { LocationService } from '../../services/location.service';
+import { Location } from '../../dtos/location';
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
+import { ArtistService } from '../../services/artist.service';
+import { Artist } from '../../dtos/artist';
 
 @Component({
   selector: 'app-event-overview',
@@ -12,14 +16,18 @@ import {NgForOf, NgIf} from "@angular/common";
   templateUrl: './event-overview.component.html',
   imports: [
     NgIf,
-    NgForOf
+    NgForOf,
+    DatePipe
   ]
 })
 export class EventOverviewComponent implements OnInit {
   event: Event | null = null;
   shows: Show[] = [];
+  location: Location | null = null;
+  artistMap: { [showId: number]: Artist[] } = {};
 
-  constructor(private route: ActivatedRoute, private eventService: EventService) {}
+
+  constructor(private route: ActivatedRoute, private eventService: EventService, private locationService: LocationService, private artistService: ArtistService) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -27,8 +35,25 @@ export class EventOverviewComponent implements OnInit {
       next: (data: EventWithShows) => {
         this.event = data.event;
         this.shows = data.shows;
+
+        this.locationService.getLocationById(data.event.locationId).subscribe({
+          next: loc => this.location = loc,
+          error: err => console.error('Failed to load location', err)
+        });
+
+        this.shows.forEach(show => {
+          this.artistMap[show.id] = [];
+
+          show.artistIds.forEach(artistId => {
+            this.artistService.getArtistById(artistId).subscribe({
+              next: artist => this.artistMap[show.id].push(artist),
+              error: err => console.error(`Failed to load artist ${artistId} for show ${show.id}`, err)
+            });
+          });
+        });
       },
       error: (err) => console.error('Failed to load event overview', err)
     });
+
   }
 }
