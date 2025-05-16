b4ee2f6eb64f45de3cc73c3882ab2eee56f56bfd
12312839 <e12312839@student.tuwien.ac.at>
2025-05-16 23:37:19 +0200

    #205 - Unused imports entfernt, style nochmal geändert

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ShowEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ShowEndpoint.java
@@ -21,14 +21,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
 import java.lang.invoke.MethodHandles;
 import java.util.List;
 
