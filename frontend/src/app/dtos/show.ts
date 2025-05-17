b4c5d1b6fe1a65e211df0a41c8d9d229cf0b01bc
12312839 <e12312839@student.tuwien.ac.at>
2025-05-17 04:51:07 +0200

    #207 - Service, Show und Component für die Suche erweitert

--- frontend/src/app/dtos/show.ts
+++ frontend/src/app/dtos/show.ts
@@ -5,4 +5,28 @@ export interface Show {
   date: string; // Format: "YYYY-MM-DDThh:mm:ss", e.g. "2023-10-01T20:00:00"
   eventId: number;
   artistIds: number[];
+  roomId: number;
+}
+
+export interface ShowSearch {
+  page: number;
+  size: number;
+  name?: string;
+  eventName?: string;
+  roomName?: string;
+  startDate?: string;
+  endDate?: string;
+  minPrice?: number;
+  maxPrice?: number;
+}
+
+export interface ShowSearchResult {
+  id: number;
+  name: string;
+  duration: number;
+  date: string;
+  eventId: number;
+  eventName: string;
+  roomId: number;
+  roomName: string;
 }
