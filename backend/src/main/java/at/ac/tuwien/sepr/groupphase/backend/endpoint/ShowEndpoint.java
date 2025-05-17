a0efd77a83845a2234d4df26e413d5dcba212901
12312839 <e12312839@student.tuwien.ac.at>
2025-05-17 04:02:32 +0200

    #206 - Endpoint für Suche erstellt

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ShowEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ShowEndpoint.java
@@ -2,9 +2,12 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.show.CreateShowDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.show.ShowDetailDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.show.ShowSearchDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.show.ShowSearchResultDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.ShowMapper;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Show;
 import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
+import at.ac.tuwien.sepr.groupphase.backend.service.SearchService;
 import at.ac.tuwien.sepr.groupphase.backend.service.ShowService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
 import java.lang.invoke.MethodHandles;
 import java.util.List;
 
@@ -30,11 +34,13 @@ public class ShowEndpoint {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final ShowService showService;
     private final ShowMapper showMapper;
+    private final SearchService searchService;
 
     @Autowired
-    public ShowEndpoint(ShowService showService, ShowMapper showMapper) {
+    public ShowEndpoint(ShowService showService, ShowMapper showMapper, SearchService searchService) {
         this.showService = showService;
         this.showMapper = showMapper;
+        this.searchService = searchService;
     }
 
     @GetMapping("/{id}")
@@ -63,5 +69,14 @@ public class ShowEndpoint {
         LOGGER.info("POST /api/v1/shows");
         Show show = showService.createShow(showMapper.createShowDtoToShow(createShowDto));
         return showMapper.showToShowDetailDto(show);
+    }
+
+    @PostMapping("/search")
+    @Secured("ROLE_ADMIN")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Search for shows by various filter criteria", security = @SecurityRequirement(name = "apiKey"))
+    public Page<ShowSearchResultDto> searchShows(@RequestBody @Valid ShowSearchDto searchDto) throws ValidationException {
+        LOGGER.info("POST /api/v1/shows/search with criteria: {}", searchDto);
+        return searchService.searchShows(searchDto);
     }
 }
