d69de439c3d63505fee35f21a1305c9f68b78fec
Felix Uhmann <uhmann.fe@gmail.com>
2025-05-18 20:00:57 +0200

    #213 - feat: created entities that are needed for ticket creation

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/ticket/Order.java
@@ -0,0 +1,55 @@
+package at.ac.tuwien.sepr.groupphase.backend.entity.ticket;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "created_at", nullable = false, updatable = false)
+    LocalDateTime createdAt;
+
+    Long userId;
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime creationDate) {
+        this.createdAt = creationDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    List<Ticket> tickets;
+}
