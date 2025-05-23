20c0493fdd279246e315ff8bbf0fb96f8b7a3418
12312839 <e12312839@student.tuwien.ac.at>
2025-05-23 19:37:51 +0200

    #246 - Fehler mit LazyLoading und Fetch gelöst

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ticket/OrderRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ticket/OrderRepository.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.sepr.groupphase.backend.repository.ticket;
 
 import at.ac.tuwien.sepr.groupphase.backend.config.type.OrderType;
 import at.ac.tuwien.sepr.groupphase.backend.entity.ticket.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,27 +10,37 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("""
-        SELECT o FROM Order o
+        SELECT o.id FROM Order o
         WHERE o.userId = :userId
           AND o.orderType = :orderType
-          AND (:past = true AND EXISTS (
-                SELECT t FROM o.tickets t WHERE t.show.date < :now
-              ) OR :past = false AND EXISTS (
-                SELECT t FROM o.tickets t WHERE t.show.date >= :now
-              ))
+          AND ((:past = true AND EXISTS (
+                SELECT t FROM o.tickets t WHERE t.show.date < :now))
+            OR (:past = false AND EXISTS (
+                SELECT t FROM o.tickets t WHERE t.show.date >= :now)))
         """)
-    Page<Order> findOrdersByTypeAndPast(
+    Page<Long> findOrderIdsByTypeAndPast(
         @Param("userId") Long userId,
         @Param("orderType") OrderType orderType,
         @Param("past") boolean past,
         @Param("now") LocalDateTime now,
         Pageable pageable
     );
+
+    @Query("""
+            SELECT DISTINCT o FROM Order o
+            JOIN FETCH o.tickets t
+            JOIN FETCH t.show s
+            JOIN FETCH s.room r
+            JOIN FETCH r.eventLocation
+            WHERE o.id IN :ids
+        """)
+    List<Order> findAllWithDetailsByIdIn(@Param("ids") List<Long> ids);
 
 
 }
