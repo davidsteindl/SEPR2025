0f7b3e6e505e1a1358a7a273f7abafa7dc2caa1f
TheSte1n <davidsteindl0@gmail.com>
2025-05-25 13:56:05 +0200

    #251 update-events.component zum Bearbeiten von Veranstaltungen hinzugefügt

---
+++ frontend/src/app/components/update-events/update-events.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-events',
+  imports: [],
+  templateUrl: './update-events.component.html',
+  styleUrl: './update-events.component.scss'
+})
+export class UpdateEventsComponent {
+
+}
