99e581882da504b5ff81b5bf9b6e05f3971a0f8b
TheSte1n <davidsteindl0@gmail.com>
2025-05-25 14:36:12 +0200

    #251 Event Anzeige in update-event component hinzugefügt

--- frontend/src/app/components/update-events/update-events.component.ts
+++ frontend/src/app/components/update-events/update-events.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { NgForOf } from '@angular/common';
+import { EventService } from '../../services/event.service';
+import { Event } from '../../dtos/event';
 
 @Component({
   selector: 'app-update-events',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgForOf,
+    RouterLink
+  ],
   templateUrl: './update-events.component.html',
   styleUrl: './update-events.component.scss'
 })
-export class UpdateEventsComponent {
+export class UpdateEventsComponent implements OnInit {
+  events: Event[] = [];
+  loading = false;
+  error: string = null;
 
+  constructor(private eventService: EventService) {}
+
+  ngOnInit(): void {
+    this.loadEvents();
+  }
+
+  loadEvents(): void {
+    this.loading = true;
+    this.eventService.getAll().subscribe({
+      next: evs => {
+        this.events = evs;
+        this.loading = false;
+      },
+      error: err => {
+        this.error = 'Konnte Events nicht laden';
+        console.error(err);
+        this.loading = false;
+      }
+    });
+  }
 }
