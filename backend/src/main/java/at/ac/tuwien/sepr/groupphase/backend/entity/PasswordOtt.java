accf634638b5bdc634b2772d027b522545bda234
Markus <5242@htl.rennweg.at>
2025-05-30 23:35:40 +0200

    #276 - Entity bearbeitet (@Getter & @Setter gelöscht und durch Methoden ausgetauscht)

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/PasswordOtt.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/PasswordOtt.java
@@ -10,12 +10,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * A class representing a Password-Reset One Time Token.
  */
-@Getter
-@Setter
 @Entity
 public class PasswordOtt {
 
@@ -35,4 +34,69 @@ public class PasswordOtt {
     @NotNull
     private boolean consumed;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isConsumed() {
+        return consumed;
+    }
+
+    public void setConsumed(boolean consumed) {
+        this.consumed = consumed;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    public void setValidUntil(LocalDateTime validUntil) {
+        this.validUntil = validUntil;
+    }
+
+    public String getOtToken() {
+        return otToken;
+    }
+
+    public void setOtToken(String otToken) {
+        this.otToken = otToken;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PasswordOtt that = (PasswordOtt) o;
+        return consumed == that.consumed && Objects.equals(id, that.id) && Objects.equals(userId, that.userId) &&
+            Objects.equals(otToken, that.otToken) && Objects.equals(validUntil, that.validUntil);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, otToken, validUntil, consumed);
+    }
+
+    @Override
+    public String toString() {
+        return "PasswordOtt{"
+            + "id=" + id
+            + ", userId=" + userId
+            + ", otToken='" + otToken + '\''
+            + ", validUntil=" + validUntil
+            + ", consumed=" + consumed
+            + '}';
+    }
 }
