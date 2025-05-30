11911edf2dc3248103bd614d2a572a221b03a32a
Markus <5242@htl.rennweg.at>
2025-05-30 20:52:15 +0200

    #279 - PasswordOtt Entity hinzugefügt für die einzlenen Token

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/PasswordOtt.java
@@ -0,0 +1,32 @@
+package at.ac.tuwien.sepr.groupphase.backend.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * A class representing a Password-Reset One Time Token.
+ */
+@Getter
+@Setter
+@Entity
+public class PasswordOtt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private LocalDateTime validUntil;
+
+}
