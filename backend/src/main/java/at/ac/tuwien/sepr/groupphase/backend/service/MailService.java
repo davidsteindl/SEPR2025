43554584d34c5bd6dc6d8547b440c127c29853bc
Markus <5242@htl.rennweg.at>
2025-05-30 20:09:44 +0200

    #279 - kleine Dateienänderung

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/MailService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/MailService.java
@@ -1,4 +1,9 @@
 package at.ac.tuwien.sepr.groupphase.backend.service;
 
+import org.springframework.mail.javamail.JavaMailSender;
+
+
 public interface MailService {
+
+
 }
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/MailServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/MailServiceImpl.java
@@ -1,4 +1,39 @@
 package at.ac.tuwien.sepr.groupphase.backend.service.impl;
 
-public class MailServiceImpl implements MailService{
+
+import at.ac.tuwien.sepr.groupphase.backend.service.MailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.lang.invoke.MethodHandles;
+
+@Service
+public class MailServiceImpl implements MailService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private final JavaMailSender javaMailSender;
+
+    public MailServiceImpl(JavaMailSender mailSender) {
+        this.javaMailSender = mailSender;
+    }
+
+
+    @Async
+    public void sendMail(String to, String subject, String text) {
+        LOGGER.info("Sending mail...");
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(to);
+        message.setFrom("noreply@gmail.com");
+        message.setSubject(subject);
+        message.setText(text);
+
+        javaMailSender.send(message);
+    }
+
 }
