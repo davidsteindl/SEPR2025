ffd9aaadaac1e9312ac25e20130dd7a8de320d2d
carolasperger <e1526751@student.tuwien.ac.at>
2025-05-31 07:46:33 +0200

    #278 - Endpoint für viewTicketPdf und Anpassung im export Ticket des Backends.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PdfExportEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PdfExportEndpoint.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 
+import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
 import at.ac.tuwien.sepr.groupphase.backend.service.PdfExportService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/pdf-export")
@@ -27,13 +30,32 @@ public class PdfExportEndpoint {
     @GetMapping("/tickets/{ticketId}")
     @Secured("ROLE_USER")
     @Operation(summary = "Generate a Ticket PDF.", security = @SecurityRequirement(name = "apiKey"))
-    public void exportTicketPdf(@PathVariable("ticketId") Long ticketId, HttpServletResponse response) throws IOException {
+    public void exportTicketPdf(@PathVariable("ticketId") Long ticketId, HttpServletResponse response) throws
+        IOException, ValidationException {
 
-        pdfExportService.makeTicketPdf(ticketId, response.getOutputStream());
+        pdfExportService.makeTicketPdf(ticketId, response.getOutputStream(), Optional.empty());
         response.setContentType("application/pdf");
         response.setHeader("Content-Disposition", "attachment; filename=Ticket" + ".pdf");
 
 
+    }
+
+    @GetMapping(value = "/tickets/{ticketId}/{randomTicketCode}")
+    @Secured("ROLE_ADMIN")
+    @Operation(summary = "View a Ticket PDF in the browser.", security = @SecurityRequirement(name = "apiKey"))
+    public void viewTicketPdf(@PathVariable("ticketId") Long ticketId, HttpServletResponse response,
+                              @PathVariable("randomTicketCode") String randomTicketCode) throws IOException, ValidationException {
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "inline; filename=Ticket" + ticketId + ".pdf");
+
+        try (OutputStream out = response.getOutputStream()) {
+            pdfExportService.makeTicketPdf(ticketId, out, Optional.of(randomTicketCode));
+            out.flush();
+        } catch (IOException | ValidationException e) {
+            response.setContentType(null);
+            response.setHeader("Content-Disposition", null);
+            throw e;
+        }
     }
 
     @GetMapping("/invoice/{orderId}")
@@ -59,8 +81,5 @@ public class PdfExportEndpoint {
 
 
     }
-
-
-
 
 }
