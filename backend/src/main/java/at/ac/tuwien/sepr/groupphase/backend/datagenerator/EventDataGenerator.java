eb54fdc7b590befe5821e2ca539018c338a2e530
TheSte1n <davidsteindl0@gmail.com>
2025-05-31 12:07:50 +0200

    #275 In EventDataGenerator Aufführungen so validiert, dass sie im korrekten Zeitrahmen einer Veranstaltung generiert werden

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/EventDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/EventDataGenerator.java
@@ -95,11 +95,18 @@ public class EventDataGenerator {
                 LocalDateTime eventStart = event.getDateTime();
                 int eventDuration = event.getDuration();
                 int showDuration = eventDuration / SHOWS_PER_ARTIST_PAIR;
-                
+
                 for (int j = 0; j < SHOWS_PER_ARTIST_PAIR; j++) {
-                    LocalDateTime showStart = eventStart.plusMinutes(j * showDuration);
+                    LocalDateTime showStart = eventStart.plusMinutes((long) j * showDuration);
+                    LocalDateTime showEnd = showStart.plusMinutes(showDuration);
+                    LocalDateTime eventEnd = eventStart.plusMinutes(eventDuration);
+                    if (showEnd.isAfter(eventEnd)) {
+                        showStart = eventEnd.minusMinutes(showDuration);
+                    }
+
                     var a1 = artists.get(artistIndex % artists.size());
                     var a2 = artists.get((artistIndex + 1) % artists.size());
+
                     Show show = Show.ShowBuilder.aShow()
                         .withName(event.getName() + " - Show " + j)
                         .withDuration(showDuration)
