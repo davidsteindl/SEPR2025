6cbca012192cf303bbd5a1fb56e8bcf869314d54
12312839 <e12312839@student.tuwien.ac.at>
2025-06-01 21:27:55 +0200

    #283 - OrderGroupRepository und Service für Detailseite der Bestellübersicht

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/TicketService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/TicketService.java
@@ -182,4 +182,5 @@ public interface TicketService {
      */
     Page<OrderGroupDto> getOrderGroupsForUser(OrderGroupType category, Pageable pageable);
 
+    OrderGroupDto getOrderGroupDetails(Long orderGroupId);
 }
