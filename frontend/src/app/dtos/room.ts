f29dd25a2c1bc3bd46ca1d6e1dc3495cab7dcc22
Felix Uhmann <uhmann.fe@gmail.com>
2025-06-01 15:08:31 +0200

    #271 - fix: added missing fields for room and sector

--- frontend/src/app/dtos/room.ts
+++ frontend/src/app/dtos/room.ts
@@ -1,8 +1,12 @@
 import {Sector} from "./sector";
 import {Location} from "./location";
+import { Seat } from "./seat";
 
 export interface Room {
   id: number;
+  xSize: number;
+  ySize: number;
+  seats: Seat[];
   sectors: Sector[];
   name: string;
 }
