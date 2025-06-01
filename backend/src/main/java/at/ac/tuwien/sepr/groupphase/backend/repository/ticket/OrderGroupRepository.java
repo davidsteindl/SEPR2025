1bd5646e0e05f6dc434a45ed9bec61889f818f15
12312839 <e12312839@student.tuwien.ac.at>
2025-06-01 11:24:35 +0200

    #283 - OrderGroupType Enum erstellt, Query geschrieben für die Bestellübersicht

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ticket/OrderGroupRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ticket/OrderGroupRepository.java
@@ -1,9 +1,41 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository.ticket;
 
 import at.ac.tuwien.sepr.groupphase.backend.entity.ticket.OrderGroup;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderGroupRepository extends JpaRepository<OrderGroup, Long> {
+
+    @Query("""
+            SELECT DISTINCT og FROM OrderGroup og
+            JOIN og.orders o
+            JOIN o.tickets t
+            JOIN t.show s
+            WHERE og.userId = :userId
+            AND o.orderType != at.ac.tuwien.sepr.groupphase.backend.config.type.OrderType.CANCELLATION
+            AND (
+                (:isReservation = TRUE AND o.orderType = at.ac.tuwien.sepr.groupphase.backend.config.type.OrderType.RESERVATION)
+                OR
+                (:isReservation = FALSE AND o.orderType IN (
+                    at.ac.tuwien.sepr.groupphase.backend.config.type.OrderType.ORDER,
+                    at.ac.tuwien.sepr.groupphase.backend.config.type.OrderType.REFUND
+                )
+                AND (
+                    (:past = TRUE AND s.date < CURRENT_TIMESTAMP)
+                    OR (:past = FALSE AND s.date >= CURRENT_TIMESTAMP)
+                ))
+            )
+        """)
+    Page<OrderGroup> findByCategory(
+        @Param("userId") Long userId,
+        @Param("isReservation") boolean isReservation,
+        @Param("past") boolean past,
+        Pageable pageable
+    );
+
 }
