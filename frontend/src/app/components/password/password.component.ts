38ae6303f9b9d0e447c0c92fb0f787a9472c4a43
Markus <5242@htl.rennweg.at>
2025-06-02 23:55:16 +0200

    #279 - Passwort vergessen GUI programmiert + Dynamisches Fenster mit Info zum Email-Versand

--- frontend/src/app/components/password/password.component.ts
+++ frontend/src/app/components/password/password.component.ts
@@ -1,14 +1,17 @@
 import {Component} from '@angular/core';
 import {NgIf} from "@angular/common";
 import {FormGroup, ReactiveFormsModule, UntypedFormBuilder, Validators} from "@angular/forms";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {EmailSentComponent} from "./email-sent/email-sent.component";
+
 
 @Component({
   selector: 'app-password',
   imports: [
     ReactiveFormsModule,
     NgIf,
-    RouterLink
+    RouterLink,
+    EmailSentComponent
   ],
   templateUrl: './password.component.html',
   styleUrl: './password.component.scss'
@@ -16,16 +19,28 @@ import {RouterLink} from "@angular/router";
 export class PasswordComponent {
   passwordResetForm: FormGroup;
   submitted = false;
+  emailSent = false;
+  isSubmitting = false;
 
-  constructor(private formBuilder: UntypedFormBuilder) {
+  constructor(private formBuilder: UntypedFormBuilder, private router: Router) {
     this.passwordResetForm = this.formBuilder.group({
       email: ['', [Validators.required]]
     });
   }
 
-
   resetPassword() {
     this.submitted = true;
+
+    if (this.passwordResetForm.invalid) {
+      return;
+    }
+
+    this.isSubmitting = true;
+    this.emailSent = true;
+
+
+    this.passwordResetForm.reset();
+    this.submitted = false;
   }
 
 }
