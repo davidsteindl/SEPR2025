ff7149c54336a9a6f2165df164b8c7068bec21b6
TheSte1n <davidsteindl0@gmail.com>
2025-06-03 15:26:32 +0200

    #275 @FutureOrPresent Annotation in EVent und Show Entitäten und DTOs entfernt, stattdessen Validierung in Event- und ShowValidator hinzugefügt

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/Show.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/Show.java
@@ -10,7 +10,6 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
