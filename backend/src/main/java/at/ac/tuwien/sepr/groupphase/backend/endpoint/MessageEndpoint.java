1dcfe509295180715bb4864e2c8234a8a98df652
carolasperger <e1526751@student.tuwien.ac.at>
2025-06-05 22:52:00 +0200

    # - News: Message Endpoint create im Backend.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/MessageEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/MessageEndpoint.java
@@ -12,16 +12,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import jakarta.validation.Valid;
 import java.lang.invoke.MethodHandles;
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(value = "/api/v1/messages")
@@ -57,9 +52,11 @@ public class MessageEndpoint {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     @Operation(summary = "Publish a new message", security = @SecurityRequirement(name = "apiKey"))
-    public DetailedMessageDto create(@Valid @RequestBody MessageInquiryDto messageDto) {
+    public DetailedMessageDto create(@RequestPart(value = "message") MessageInquiryDto messageDto,
+        @RequestPart(value = "images", required = false) List<MultipartFile> files) {
+
         LOGGER.info("POST /api/v1/messages body: {}", messageDto);
         return messageMapper.messageToDetailedMessageDto(
-            messageService.publishMessage(messageMapper.messageInquiryDtoToMessage(messageDto)));
+            messageService.publishMessage(messageMapper.toMessage(messageDto, files)));
     }
 }
