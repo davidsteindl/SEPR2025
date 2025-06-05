75873b8c97ccb3695d4296f9664118e1e58158ee
TheSte1n <davidsteindl0@gmail.com>
2025-06-06 00:14:33 +0200

    #275 In UserDataGenerator 1000 Test- Kunden und 50 Test- Admins erstellt

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/UserDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/UserDataGenerator.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.sepr.groupphase.backend.datagenerator;
 
 import java.lang.invoke.MethodHandles;
 import java.time.LocalDate;
+
 import at.ac.tuwien.sepr.groupphase.backend.config.type.Sex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,10 +23,6 @@ public class UserDataGenerator {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    private static final String USER_EMAIL = "user@email.com";
-    private static final String ADMIN_EMAIL = "admin@email.com";
-
-    private static final String PASSWORD = "password";
 
     @Autowired
     public UserDataGenerator(UserRepository userRepository, PasswordEncoder passwordEncoder) {
@@ -36,47 +33,53 @@ public class UserDataGenerator {
     @PostConstruct
     private void generateUsers() {
         if (!userRepository.findAll().isEmpty()) {
-            LOGGER.debug("users already generated");
+            LOGGER.debug("Users already generated");
         } else {
-            LOGGER.debug("generating users");
-            ApplicationUser admin = ApplicationUserBuilder.aUser()
-                .withEmail(ADMIN_EMAIL)
-                .withPassword(passwordEncoder.encode(PASSWORD))
-                .withFirstName("Max")
-                .withLastName("Mustermann")
-                .withDateOfBirth(LocalDate.of(1972, 2, 3))
-                .withSex(Sex.MALE)
-                .isLocked(false)
-                .isAdmin(true)
-                .withLoginTries(0)
-                .build();
-            userRepository.save(admin);
+            LOGGER.debug("Generating 50 admins and 1000 customers");
+            for (int i = 1; i <= 50; i++) {
+                String email = String.format("admin" + i + "@email.com");
+                String firstName = "AdminFirstName" + i;
+                String lastName = "AdminLastName" + i;
+                Sex sex = (i % 2 == 0) ? Sex.FEMALE : Sex.MALE;
+                LocalDate dob = LocalDate.of(1970, 1, 1).plusDays(i);
 
-            ApplicationUser user = ApplicationUserBuilder.aUser()
-                .withEmail(USER_EMAIL)
-                .withPassword(passwordEncoder.encode(PASSWORD))
-                .withFirstName("Magdalena")
-                .withLastName("Musterfrau")
-                .withDateOfBirth(LocalDate.of(1980, 7, 15))
-                .withSex(Sex.FEMALE)
-                .isLocked(false)
-                .isAdmin(false)
-                .withLoginTries(0)
-                .build();
-            userRepository.save(user);
+                ApplicationUser admin = ApplicationUserBuilder.aUser()
+                    .withEmail(email)
+                    .withPassword(passwordEncoder.encode("password" + i))
+                    .withFirstName(firstName)
+                    .withLastName(lastName)
+                    .withDateOfBirth(dob)
+                    .withSex(sex)
+                    .isLocked(false)
+                    .isAdmin(true)
+                    .withLoginTries(0)
+                    .build();
+                userRepository.save(admin);
+            }
 
-            ApplicationUser lockedUser = ApplicationUserBuilder.aUser()
-                .withEmail("locked@email.com")
-                .withPassword(passwordEncoder.encode(PASSWORD))
-                .withFirstName("Locked")
-                .withLastName("User")
-                .withDateOfBirth(LocalDate.of(1980, 7, 15))
-                .withSex(Sex.MALE)
-                .isLocked(true)
-                .isAdmin(false)
-                .withLoginTries(5)
-                .build();
-            userRepository.save(lockedUser);
+            for (int i = 1; i <= 1000; i++) {
+                String email = String.format("user" + i + "email.com");
+                String firstName = "UserFirstName" + i;
+                String lastName = "UserLastName" + i;
+
+                Sex sex = (i % 2 == 0) ? Sex.FEMALE : Sex.MALE;
+                LocalDate dob = LocalDate.of(1990, 1, 1).plusDays(i);
+
+                ApplicationUser customer = ApplicationUserBuilder.aUser()
+                    .withEmail(email)
+                    .withPassword(passwordEncoder.encode("password" + i))
+                    .withFirstName(firstName)
+                    .withLastName(lastName)
+                    .withDateOfBirth(dob)
+                    .withSex(sex)
+                    .isLocked(false)
+                    .isAdmin(false)
+                    .withLoginTries(0)
+                    .build();
+                userRepository.save(customer);
+            }
+
+            LOGGER.debug("Finished generating 50 admins and 1000 customers");
         }
     }
 }
