9a1ef8bc04f856b4e10aee119fd843e3c45ba7e2
TheSte1n <davidsteindl0@gmail.com>
2025-06-05 23:43:26 +0200

    #275 OrderDataGenerator erstellt jetzt vergangene Bestelungen für vergangene Events

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ticket/TicketRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ticket/TicketRepository.java
@@ -1,10 +1,17 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository.ticket;
 
+import at.ac.tuwien.sepr.groupphase.backend.config.type.TicketStatus;
 import at.ac.tuwien.sepr.groupphase.backend.entity.ticket.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findByShowId(Long showId);
+
+    Page<Ticket> findByOrderId(Long orderId, Pageable pageable);
+
+    long countByStatus(TicketStatus status);
 }
