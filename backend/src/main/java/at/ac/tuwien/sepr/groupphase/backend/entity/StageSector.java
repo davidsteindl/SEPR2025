7543503a9339aa0930e1bd5ff9fb2638254c0368
Filip Grgic <e12324225@student.tuwien.ac.at>
2025-06-05 03:50:37 +0200

    #277 - Checkstyle Fehler ausgebessert

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/StageSector.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/StageSector.java
@@ -3,7 +3,7 @@ package at.ac.tuwien.sepr.groupphase.backend.entity;
 import jakarta.persistence.Entity;
 
 @Entity
-public class StageSector extends Sector{
+public class StageSector extends Sector {
     @Override
     public boolean isBookable() {
         return false;
