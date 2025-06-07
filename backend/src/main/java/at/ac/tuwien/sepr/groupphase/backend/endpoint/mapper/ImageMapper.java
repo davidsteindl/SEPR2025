587b979c453570539839778eed60cd5ea2d978c8
carolasperger <e1526751@student.tuwien.ac.at>
2025-06-07 23:25:58 +0200

    #286 - ImageMapper im Backend.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ImageMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ImageMapper.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper;
 
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.message.ImageDto;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Image;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -14,12 +15,12 @@ public interface ImageMapper {
 
     ImageMapper INSTANCE = Mappers.getMapper(ImageMapper.class);
 
-    // Mapping für eine Liste von MultipartFile zu einer Liste von Images
     List<Image> toImageList(List<MultipartFile> multipartFiles);
 
-    // Einzelnes MultipartFile zu Image mappen
-    @Mapping(target = "id", ignore = true) // ID wird von der Datenbank generiert
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "image", expression = "java(file.getBytes())")
     @Mapping(target = "imageType", expression = "java(file.getContentType())")
     Image toImage(MultipartFile file) throws IOException;
+
+    ImageDto toImageDto(Image image);
 }
