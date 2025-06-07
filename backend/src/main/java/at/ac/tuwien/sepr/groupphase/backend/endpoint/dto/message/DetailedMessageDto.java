ee14b861b76aa2c441d699a2785903fc6f4c8526
carolasperger <e1526751@student.tuwien.ac.at>
2025-06-07 23:31:23 +0200

    #286 - DetailedMessageDto Image Teile Backend.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/message/DetailedMessageDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/message/DetailedMessageDto.java
@@ -1,16 +1,25 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.message;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 public class DetailedMessageDto extends SimpleMessageDto {
 
-    //Liste an Bildern soll ImageDto
+    private List<ImageDto> images;
 
     private String text;
 
     public String getText() {
         return text;
+    }
+
+    public List<ImageDto> getImages() {
+        return images;
+    }
+
+    public void setImages(List<ImageDto> images) {
+        this.images = images;
     }
 
     public void setText(String text) {
