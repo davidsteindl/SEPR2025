ea7b7386aaad7a8175b9eaff3b8a75c60b194fb6
TheSte1n <davidsteindl0@gmail.com>
2025-06-18 22:18:23 +0200

    #326 In RoomEndpoint Sortierungslogik nach eventLocation name und room name hinzugefügt

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/RoomEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/RoomEndpoint.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -86,7 +87,13 @@ public class RoomEndpoint {
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "10") int size) {
         LOGGER.info("GET /api/v1/rooms/page?page={}&size={}", page, size);
-        Pageable pageable = PageRequest.of(page, size);
-        return roomservice.getAllRoomsPaginated(pageable);
+        Pageable sorted = PageRequest.of(page,
+            size,
+            Sort.by(
+                Sort.Order.asc("eventLocation.name").ignoreCase(),
+                Sort.Order.asc("name").ignoreCase()
+            )
+        );
+        return roomservice.getAllRoomsPaginated(sorted);
     }
 }
