cd9986580985ea6cbdfc40442c3d9fbb3687b0ac
carolasperger <e1526751@student.tuwien.ac.at>
2025-06-22 10:30:53 +0200

    #355 - Backend: Checkstyle Korrektur.

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/unittests/Service/ImageServiceImplTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/unittests/Service/ImageServiceImplTest.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class ImageServiceImplTest {
